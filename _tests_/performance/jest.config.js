e4e0bda720809bd10e7e4825f20fddead965a39e
Parkkangwon <41990830+Parkkangwon@users.noreply.github.com>
2025-09-03 23:34:42 +0900

    Create jest.config.js

---
+++ _tests_/performance/jest.config.js
@@ -0,0 +1,13 @@
+// Performance test specific Jest configuration
+module.exports = {
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/../../$1',
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  testMatch: ['**/*.performance.test.tsx'],
+  moduleDirectories: ['node_modules', '<rootDir>/../../'],
+};
